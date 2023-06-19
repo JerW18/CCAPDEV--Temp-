ddc1f935e048594d2b0f7fd6b937ccae8d605a96
Jeremy Wang <jeremywang512@gmail.com>
2023-06-19 17:57:27 +0800

    Lanz's search profile

--- js/search_profile.js
+++ js/search_profile.js
@@ -2,29 +2,84 @@
 //add rows to the reservation information column when there is a reservation,
 //make it scrollable.
 
-let reservations=[
+let reservations = [
     {
-        res_code: "R#10104", 
-        room: "G304",
-        seat: 010,
-        requested:[{year:"2023",month:"6",day:"19",time:"10:34"}],
-        reserved:[{year:"2023",month:"6",day:"23",start:"",end:""}]
-    },
-    {
-        res_code: "R#10105", 
+        res_code: "R#10104",
         room: "G304",
         seat: "010",
-        requested:[{year:"2023",month:"6",day:"19",time:"10:34"}],
-        reserved:[{year:"2023",month:"6",day:"24",start:"",end:""}]
+        requested: "2023-6-19",
+        reserved: "2023-6-23",
+        start: "20", 
+        end: "26" 
     },
     {
-        res_code: "R#10104", 
+        res_code: "R#10105",
         room: "G304",
-        seat: 10,
-        requested:[{year:"2023",month:"6",day:"19",time:"10:34"}],
-        reserved:[{year:"2023",month:"6",day:"23",start:"",end:""}]
-    }
+        seat: "010",
+        requested: "2023-6-19",
+        reserved: "2023-6-24",
+        start: "20",
+        end: "26"
+    },
+    {
+        res_code: "R#10104",
+        room: "G304",
+        seat: "010",
+        requested: "2023-6-19",
+        reserved: "2023-6-25",  
+        start: "20", 
+        end: "26"
+    },
 ];
-function initializeResTable(){
 
+
+
+function initializeResTable() {
+  //one reservation=1 row
+  //one detail of reservation=1col
+    let insert = "";
+    const table = document.getElementById("table");
+    
+    for (let i of reservations) {
+        insert += "<tr>";
+        for (const j in i) {
+            insert += "<th>"
+            //to print and format time
+            if(j == "start" || j == "end"){
+                let time = parseInt(i[j]);
+                let morning=true    
+                let minutes = time % 2;
+                time/=2;
+                time=Math.floor(time);
+                if(time>12){
+                    time-=12;
+                    morning=false;
+                }
+                time = time.toString();
+                if(minutes == 0){
+                    time += ":00";
+                }
+                else{
+                    time += ":30";
+                }
+                if(!morning){
+                    time+=" PM";
+                }
+                else{
+                    time+=" AM";
+                }
+                insert += time;
+            }
+
+            else{
+                insert += i[j];    
+            }
+            
+            insert+="</th>"
+        }
+        insert += "</tr>";
+    }
+    table.innerHTML+=insert;
 }
+
+initializeResTable();
