e6fc6b463c33f7c5e25a05cb580543a7f704d375
Jeremy Wang <jeremywang512@gmail.com>
2023-06-22 14:46:46 +0800

    Fixed guest reserve navbar

--- js/temp_reserve_guest.js
+++ js/temp_reserve_guest.js
@@ -45,13 +45,9 @@ initializeNavBarButtons();
 
 /* initialize navbar buttons*/
 function initializeNavBarButtons(){
-    const searchButton=document.getElementById("search");
-    const profileButton=document.getElementById("profile");
-    searchButton.addEventListener("click",(e)=>{
-        window.location.assign("search_profile.html");
-    })
-    profileButton.addEventListener("click",(e)=>{
-        window.location.assign("student_profile.html");
+    const loginButton = document.getElementById("login");
+    loginButton.addEventListener("click",(e)=>{
+        window.location.assign("../index.html");
     })
 }
 
